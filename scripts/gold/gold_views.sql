fe80b8cee9786c8a8de8e41307a5e96c0e45dffd
Will <wibrigham9890@gmail.com>
2025-07-11 14:07:32 -0400

    Create Gold Views Pt 1

---
+++ scripts/gold/gold_views.sql
@@ -0,0 +1,44 @@
+/*
+===============================================================================
+DDL Script: Create Gold Views
+===============================================================================
+Script Purpose:
+    This script creates views for the Gold layer in the data warehouse. 
+    The Gold layer represents the final dimension and fact tables (Star Schema)
+
+    Each view performs transformations and combines data from the Silver layer 
+    to produce a clean, enriched, and business-ready dataset.
+
+Usage:
+    - These views can be queried directly for analytics and reporting.
+===============================================================================
+*/
+
+-- =============================================================================
+-- Create Dimension: gold.dim_customers
+-- =============================================================================
+IF OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL
+    DROP VIEW gold.dim_customers;
+GO
+
+CREATE VIEW gold.dim_customers AS
+SELECT
+    ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key, -- Surrogate key
+    ci.cst_id                          AS customer_id,
+    ci.cst_key                         AS customer_number,
+    ci.cst_firstname                   AS first_name,
+    ci.cst_lastname                    AS last_name,
+    la.cntry                           AS country,
+    ci.cst_marital_status              AS marital_status,
+    CASE 
+        WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the primary source for gender
+        ELSE COALESCE(ca.gen, 'n/a')  			   -- Fallback to ERP data
+    END                                AS gender,
+    ca.bdate                           AS birthdate,
+    ci.cst_create_date                 AS create_date
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca
+    ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+    ON ci.cst_key = la.cid;
+GO
